62f01854495340e60d4297ec456114b3092fd268
Tim <tjk315@gmail.com>
2018-12-25 16:11:02 -0500

    Add Dummy Data, new ObjectType, RootQuery ("single" & "all" queries)

--- schema.js
+++ schema.js
@@ -7,6 +7,57 @@ const {
   GraphQLNonNull,
 } = require('graphql');
 
+// Hardcoded Data - For Demo Purposes
+const customers = [
+  { id: '1', name: 'John Doe', email: 'jdoe@gmail.com', age: 35 },
+  { id: '2', name: 'Steve Smith', email: 'steve@gmail.com', age: 25 },
+  { id: '3', name: 'Sara Williams', email: 'sara@gmail.com', age: 32 }
+]
+
+// Customer Type
+const CustomerType = new GraphQLObjectType({
+  name: 'Customer',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+});
+
+// Root Query - A Required Base Query
+const RootQuery = new GraphQLObjectType({
+  // All ObjectTypes need a "name"
+  name: 'RootQueryType',
+  fields: {
+    // Query Single "customer"
+    customer: {
+      // ObjectType for RootQuery
+      type: CustomerType,
+      args: {
+        // Param to fetch by
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        // Iterate through "customers" Obj Array
+        for(let i = 0; i < customers.length; i++) {
+          // Search for customer that matches passed "id"
+          if(customers[i].id == args.id) {
+            return customers[i];
+          }
+        }
+      }
+    },
+    // Query All "customers"
+    customers: {
+      type: new GraphQLList(CustomerType),
+      resolve(parentValue, args) {
+        return customers;
+      }
+    }
+  }
+});
+
 module.exports = new GraphQLSchema({
-  
+  query: RootQuery
 });
